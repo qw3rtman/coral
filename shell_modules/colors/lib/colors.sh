423884f0f8ba27bce061496e1ad01d98ba852b2c
Nimit Kalra <nimit@nimitkalra.com>
2015-12-21 00:26:10 -0600

    Add require functionality.

---
+++ shell_modules/colors/lib/colors.sh
@@ -0,0 +1,3 @@
+turquoise() {
+  printf "\033[36m$1\033[0m"
+}
---
+++ shell_modules/colors/package.sh
@@ -0,0 +1,6 @@
+name="colors"
+version="0.0.1"
+description="colors in your terminal!"
+keywords=("colors", "ansi")
+preferGlobal=false
+main="lib/colors.sh"
